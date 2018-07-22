f728d8f4bc24c434e2f59ab6b7f0b6a42c35cbf7
Frisco Miranda <frisco.miranda@procore.com>
2018-07-22 15:00:59 -0700

    complete my_all method and a few other enumerables

--- enumerables.rb
+++ enumerables.rb
@@ -10,37 +10,75 @@ module Enumerable
   end
 
   def my_each_with_index
-    
-
-
-    # if block_given?
-    #   for element in self do
-    #     yield([element, self.index(element)])
-    #   end
-    # else
-    #   self.to_enum
-    # end
-
-    # return enum_for(__method__) { 1 } unless block_given?
-    # for element in self do
-    #   yield([element, self.index(element)])
-    # end
+    if block_given?
+      index = 0
+      for element in self do
+        yield(element, index)
+        index += 1
+      end
+    else
+      self.to_enum
+    end
   end
 
   def my_select
-
+    if block_given?
+      arr = []
+      self.my_each do |element|
+        if yield(element) == true
+          arr << element
+        end
+      end
+      return arr
+    else
+      self.to_enum
+    end
   end
 
-  def my_all?
-
+  def my_all?(arg=nil)
+    if block_given?
+      status = true
+      self.my_each do |element|
+        if !yield(element) #== false
+          status = false
+        end
+      end
+      return status
+    elsif !arg.nil?
+      status = true
+      self.my_each do |element|
+        unless arg === element
+          status = false
+        end
+      end
+      status
+    elsif !block_given? && arg.nil?
+      self.my_all? { |obj| obj }
+    end
   end
 
   def my_any?
-
+    if block_given?
+      fulfilled = true
+      self.my_each do |element|
+        if yield(element) != true
+          fulfilled = false
+        end
+      end
+      return fulfilled
+    else
+      self.my_any? { |obj| obj }
+    end
   end
 
-  def my_none?
+  def my_none?(arg = nil)
+    if arg.nil?
+      puts "gimme some stuff"
+    elsif block_given?
+      puts arg
+    else
 
+    end
   end
 
   def my_count
@@ -55,13 +93,4 @@ module Enumerable
 
   end
 
-end
-
-
-def test
-  if block_given?
-    "i got it!"
-  else
-    "No I don't!"
-  end
 end
